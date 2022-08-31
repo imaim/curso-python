5baa91a2fbf4e7f66152e3cc42932b3eb324d34c
Imaim Vilela <imaim.vilela@2comconsulting.com.br>
2022-08-31 15:53:25 -0300

    testes com import de funcao

---
+++ introducao_variavel-ambiente.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+import introducao_variavel_global_local as f
+f.local_variavel_exemplo()
---
+++ introducao_variavel_global_local.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+hacker = 'Eu'
+def local_variavel_exemplo() :
+    hacker = 'Você'
+    print ('A variavel Local é %s' %hacker)
+local_variavel_exemplo()
+
+print('A variavel Gloval é %s' %hacker)
